6de10c9a7dd5ede3c63d522ef5feedccec7eb015
Dennisbonke <dennisb@kabelfoon.net>
2015-01-19 00:01:00 +0100

    Added main class and plugin.yml

---
+++ src/main/java/com/dennisbonke/test/Test.java
@@ -0,0 +1,19 @@
+package com.dennisbonke.test;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Dennisbonke on 18-1-2015.
+ */
+public class Test extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
