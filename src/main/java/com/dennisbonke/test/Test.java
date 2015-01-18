862bb22f4d458a977aab2dd7d11346d4799351d8
Dennisbonke <dennisb@kabelfoon.net>
2015-01-19 00:36:04 +0100

    Added Chat.

--- src/main/java/com/dennisbonke/test/Test.java
+++ src/main/java/com/dennisbonke/test/Test.java
@@ -1,11 +1,18 @@
 package com.dennisbonke.test;
 
+import com.dennisbonke.test.util.Chat;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * Created by Dennisbonke on 18-1-2015.
  */
 public class Test extends JavaPlugin {
+
+    public static String prefix = ChatColor.RED + "[TEST] " + ChatColor.WHITE;
+    public static Chat chat;
+
+    private static Test test;
 
     @Override
     public void onEnable() {
---
+++ src/main/java/com/dennisbonke/test/util/Chat.java
@@ -0,0 +1,68 @@
+package com.dennisbonke.test.util;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Dennisbonke on 19-1-2015.
+ */
+public class Chat implements Listener {
+
+    private static List<Chat> allChats = new ArrayList<Chat>();
+
+    private final String plugin;
+    private final String prefix;
+
+    public Chat(String plugin, ChatColor color) {
+        this.plugin = plugin;
+        this.prefix = color + "[" + plugin + "]" + ChatColor.WHITE;
+
+        allChats.add(this);
+    }
+
+    public Chat(String plugin) {
+        this.plugin = plugin;
+        this.prefix = ChatColor.RED + "[" + plugin + "]" + ChatColor.WHITE;
+
+        allChats.add(this);
+    }
+
+    public static Chat getChat(String plugin) {
+        for (Chat chat : allChats)
+            if (chat.getName().equalsIgnoreCase(plugin))
+                    return chat;
+        return null;
+    }
+
+    private static ConsoleCommandSender getConsole() {
+        return Bukkit.getServer().getConsoleSender();
+    }
+
+    public String getName() {
+        return plugin;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void sendConsoleMessage(String message) {
+        getConsole().sendMessage(getPrefix() + message);
+    }
+
+    public void sendMessage(Player player, String message) {
+        player.sendMessage(getPrefix() + message);
+    }
+
+    public void sendMessage(CommandSender sender, String message) {
+        sender.sendMessage(getPrefix() + message);
+    }
+
+}
